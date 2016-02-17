1bfc2e0a1cba8dc633aba5f9e0280d5f70461421
Kreees <sukharevkirill@gmail.com>
2016-02-17 09:32:50 +0300

    Fix describe initialization. this.ctx is this.suit now. Fix error with first argument check

--- app.js
+++ app.js
@@ -9,24 +9,25 @@ var fRegCheck = /^function\s+\(\S+?\)/;
 
 var generateDescribe = function(list,ctx) {
     var Suit = list.shift();
-    var f = function() {
+    var describeString = String(Suit.describe);
+    if (!/\.$/.test(describeString)) describeString += ".";
 
-        this.ctx = ctx;
-        Suit.call && Suit.call.call(this);
-
+    describe(describeString,function(){
+        this.suit = ctx;
+        Suit.fcall && Suit.fcall.call(this);
 
         ["__before","__beforeEach","__after","__afterEach"].forEach(function(callName){
             var globalCall = callName.replace(/^__/,"");
             Suit[callName] && Suit[callName].forEach(function(f){
                 if (f.useDone) {
                     global[globalCall](function(done) {
-                        this.ctx = ctx;
-                        return f.call.call(this,done);
+                        this.suit = ctx;
+                        return f.fcall.call(this,done);
                     });
                 } else {
                     global[globalCall](function() {
-                        this.ctx = ctx;
-                        return f.call.call(this);
+                        this.suit = ctx;
+                        return f.fcall.call(this);
                     });
                 }
             });
@@ -36,13 +37,13 @@ var generateDescribe = function(list,ctx) {
         Suit.__it && Suit.__it.forEach(function(f){
             if (f.useDone) {
                 it(f.msg,function(done) {
-                    this.ctx = ctx;
-                    return f.call.call(this,done);
+                    this.suit = ctx;
+                    return f.fcall.call(this,done);
                 });
             } else {
                 it(f.msg,function() {
-                    this.ctx = ctx;
-                    return f.call.call(this);
+                    this.suit = ctx;
+                    return f.fcall.call(this);
                 });
             }
         });
@@ -50,16 +51,21 @@ var generateDescribe = function(list,ctx) {
         if (list.length > 0) {
             generateDescribe(list,ctx);
         }
-    };
-
-    describe(Suit.describe.toString(),function(){
-        extend(this);
-        extend(this,ctx);
-        f.apply(this,arguments);
     });
 };
 
-module.exports = function(name) {
+module.exports = function(name,f) {
+    if (!global.hasOwnProperty("describe")) {
+        throw Error("There is no 'describe' method in your global. Probably mocha isn't running.");
+    }
+
+    if (name instanceof Function) {
+        f = name;
+        name = "";
+    }
+
+    name = name || "";
+
     var TestSuit = function(_ctx){
         if (!(this instanceof this.suit)){
             return;
@@ -83,7 +89,7 @@ module.exports = function(name) {
     };
 
     var pushNewCall = function(Suit,callName,args) {
-        if (!(args.call instanceof Function)) {
+        if (!(args.fcall instanceof Function)) {
             throw Error((callName == "it"?"Second argument":"Argument")+" should be test case method");
         }
         var containerName = "__"+callName;
@@ -93,38 +99,39 @@ module.exports = function(name) {
         Suit[containerName].push(args);
     };
 
-    TestSuit.describe = name || "";
+    TestSuit.describe = name;
+    TestSuit.fcall = f;
     TestSuit.parent = null;
     TestSuit.contextData = {};
 
     TestSuit.prototype.suit = TestSuit;
 
     TestSuit.before = function(f) {
-        pushNewCall(this,"before", { call: f, useDone: fRegCheck.test(f) });
+        pushNewCall(this,"before", { fcall: f, useDone: fRegCheck.test(f) });
     };
 
     TestSuit.beforeEach = function(f) {
-        pushNewCall(this,"beforeEach", { call: f, useDone: fRegCheck.test(f) });
+        pushNewCall(this,"beforeEach", { fcall: f, useDone: fRegCheck.test(f) });
     };
 
     TestSuit.after = function(f) {
-        pushNewCall(this,"after", { call: f, useDone: fRegCheck.test(f) });
+        pushNewCall(this,"after", { fcall: f, useDone: fRegCheck.test(f) });
     };
 
     TestSuit.afterEach = function(f) {
-        pushNewCall(this,"afterEach", { call: f, useDone: fRegCheck.test(f) });
+        pushNewCall(this,"afterEach", { fcall: f, useDone: fRegCheck.test(f) });
     };
 
     TestSuit.it = function(msg,f) {
-        if (!(msg instanceof String)) {
-            throw Error("First argument should be test case description (String)");
-        }
-        pushNewCall(this,"it", { msg: msg, call: f, useDone: fRegCheck.test(f) });
+        msg = String(msg);
+        pushNewCall(this,"it", { msg: msg, fcall: f, useDone: fRegCheck.test(f) });
     };
 
     TestSuit.extend = function(msg,ctx,f) {
-        if (!(msg instanceof String)) {
-            throw Error("First argument should be suit description (String)");
+        msg = String(msg);
+        if (ctx instanceof Function) {
+            f = ctx;
+            ctx = {};
         }
         if (ctx && !(ctx instanceof Object)) {
             throw Error("Second argument should be object if set");
@@ -137,9 +144,8 @@ module.exports = function(name) {
         extend(NewCase,Parent);
         NewCase.prototype.suit = NewCase;
         NewCase.parent = Parent;
-        NewCase.call = f;
+        NewCase.fcall = f;
         NewCase.describe = msg || "";
-        if (!/\.$/.test(NewCase.describe)) NewCase.describe += ".";
         NewCase.contextData = ctx;
         return NewCase;
     };
