db3e9d4d1acf928656b9cf0756b8a1cd04f4e21d
Kreees <sukharevkirill@gmail.com>
2016-02-18 19:38:12 +0300

    add that method, x* method, and new as describe

--- app.js
+++ app.js
@@ -1,134 +1,90 @@
-var extend = function(obj1,obj2) {
-    obj2 = obj2 || {};
-    Object.keys(obj2).forEach(function(key){
-        obj1[key] = obj2[key];
-    });
-};
+'use strict';
 
 var fRegCheck = /^function\s+\(\S+?\)/;
+var generateDescribe = require("./lib/generate");
+const METHODS = generateDescribe.METHODS;
+const IT_METHODS = generateDescribe.IT_METHODS;
+const THAT_METHODS = generateDescribe.THAT_METHODS;
 
-var generateDescribe = function(list,ctx) {
-    var Suit = list.shift();
-    var describeString = String(Suit.describe);
-    if (!/\.$/.test(describeString)) describeString += ".";
+var utils = require("./lib/utils");
 
-    describe(describeString,function(){
-        this.suit = ctx;
-        Suit.fcall && Suit.fcall.call(this);
-
-        ["__before","__beforeEach","__after","__afterEach"].forEach(function(callName){
-            var globalCall = callName.replace(/^__/,"");
-            Suit[callName] && Suit[callName].forEach(function(f){
-                if (f.useDone) {
-                    global[globalCall](function(done) {
-                        this.suit = ctx;
-                        return f.fcall.call(this,done);
-                    });
-                } else {
-                    global[globalCall](function() {
-                        this.suit = ctx;
-                        return f.fcall.call(this);
-                    });
-                }
-            });
-        });
-
-
-        Suit.__it && Suit.__it.forEach(function(f){
-            if (f.useDone) {
-                it(f.msg,function(done) {
-                    this.suit = ctx;
-                    return f.fcall.call(this,done);
-                });
-            } else {
-                it(f.msg,function() {
-                    this.suit = ctx;
-                    return f.fcall.call(this);
-                });
-            }
-        });
-
-        if (list.length > 0) {
-            generateDescribe(list,ctx);
-        }
-    });
-};
-
-module.exports = function(name,f) {
+module.exports = function(name,ctx,f) {
     if (!global.hasOwnProperty("describe")) {
         throw Error("There is no 'describe' method in your global. Probably mocha isn't running.");
     }
 
     if (name instanceof Function) {
-        f = name;
+        ctx = name;
         name = "";
+    }
+
+    if (ctx instanceof Function) {
+        f = ctx;
+        ctx = {};
+    }
+
+    if (ctx && !(ctx instanceof Object)) {
+        throw Error("Second argument should be object if set");
+    }
+    if (f && !(f instanceof Function)) {
+        throw Error("Third argument should be function if set");
     }
 
     name = name || "";
 
-    var TestSuit = function(_ctx){
+    var TestSuit = function(msg,_ctx){
         if (!(this instanceof this.suit)){
             return;
         }
         else {
+            if (msg instanceof Object) {
+                _ctx = msg;
+                msg = "";
+            }
+
             var parents = [];
             var current = this.suit;
+
             while(current instanceof Function) {
                 parents.unshift(current);
                 current = current.parent;
             }
 
             var ctx = {};
+            var That = {__it: [], __xit: []};
             parents.forEach(function(Suit){
-                extend(ctx,Suit.contextData);
+                utils.extend(ctx,Suit.contextData);
+                That.__it = That.__it.concat(Suit.__that || []);
+                That.__xit = That.__xit.concat(Suit.__xthat || []);
             });
-            extend(ctx,_ctx);
 
-            generateDescribe(parents,ctx);
+            utils.extend(ctx,_ctx);
+
+            generateDescribe(parents,ctx,utils.dotEndString(msg),That);
         }
     };
 
-    var pushNewCall = function(Suit,callName,args) {
-        if (!(args.fcall instanceof Function)) {
-            throw Error((callName == "it"?"Second argument":"Argument")+" should be test case method");
-        }
-        var containerName = "__"+callName;
-        if (!(Suit[containerName] instanceof  Array)){
-            Object.defineProperty(Suit,containerName,{value: [], writable: false, enumerable: false});
-        }
-        Suit[containerName].push(args);
-    };
+    utils.extendSuit(TestSuit,{
+        parent: null,
+        fcall: f,
+        describe: name,
+        contextData: {}
+    });
 
-    TestSuit.describe = name;
-    TestSuit.fcall = f;
-    TestSuit.parent = null;
-    TestSuit.contextData = {};
+    METHODS.forEach(function(callName){
+        TestSuit[callName] = function(f) {
+            utils.pushNewCall(this,callName, { fcall: f, useDone: fRegCheck.test(f) });
+        };
+    });
 
-    TestSuit.prototype.suit = TestSuit;
-
-    TestSuit.before = function(f) {
-        pushNewCall(this,"before", { fcall: f, useDone: fRegCheck.test(f) });
-    };
-
-    TestSuit.beforeEach = function(f) {
-        pushNewCall(this,"beforeEach", { fcall: f, useDone: fRegCheck.test(f) });
-    };
-
-    TestSuit.after = function(f) {
-        pushNewCall(this,"after", { fcall: f, useDone: fRegCheck.test(f) });
-    };
-
-    TestSuit.afterEach = function(f) {
-        pushNewCall(this,"afterEach", { fcall: f, useDone: fRegCheck.test(f) });
-    };
-
-    TestSuit.it = function(msg,f) {
-        msg = String(msg);
-        pushNewCall(this,"it", { msg: msg, fcall: f, useDone: fRegCheck.test(f) });
-    };
+    IT_METHODS.concat(THAT_METHODS).forEach(function(callName){
+        TestSuit[callName] = function(msg,f) {
+            msg = String(msg);
+            utils.pushNewCall(this,callName, { msg: msg, fcall: f, useDone: fRegCheck.test(f) });
+        };
+    });
 
     TestSuit.extend = function(msg,ctx,f) {
-        msg = String(msg);
         if (ctx instanceof Function) {
             f = ctx;
             ctx = {};
@@ -139,15 +95,24 @@ module.exports = function(name,f) {
         if (f && !(f instanceof Function)) {
             throw Error("Third argument should be function if set");
         }
+
+        msg = String(msg);
         var Parent = this;
-        var NewCase = function() { Parent.apply(this,arguments); };
-        extend(NewCase,Parent);
-        NewCase.prototype.suit = NewCase;
-        NewCase.parent = Parent;
-        NewCase.fcall = f;
-        NewCase.describe = msg || "";
-        NewCase.contextData = ctx;
-        return NewCase;
+        var NewSuit = function() { Parent.apply(this,arguments); };
+        utils.extend(NewSuit,Parent);
+        utils.extendSuit(NewSuit,{
+            parent: Parent,
+            fcall: f,
+            describe: msg || "",
+            contextData: ctx
+        });
+        return NewSuit;
+    };
+
+    TestSuit.xtend = function(msg,ctx,f) {
+        var NewSuit = this.extend.apply(this,arguments);
+        Object.defineProperty(NewSuit,"skip", {value: true});
+        return NewSuit;
     };
 
     return TestSuit;
